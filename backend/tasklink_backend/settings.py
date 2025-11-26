67c36d91482c4dc881fb38877ff46ff1d6e784fa
Alisher Akbarov <100197528+VUHAYB@users.noreply.github.com>
2025-11-26 15:22:58 +0100

    Update settings.py

--- backend/tasklink_backend/settings.py
+++ backend/tasklink_backend/settings.py
@@ -135,6 +135,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import environ
+import dj_database_url
 
 # Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
